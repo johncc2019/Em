94377a328d772e37df7845a5986439b3a59b97d2
Johncc <47912198+johncc2019@users.noreply.github.com>
2021-05-19 19:40:31 +0800

    Update em.js

--- em.js
+++ em.js
@@ -1,3 +1,12 @@
+/*
+  @author: KleinerSource
+ 
+  @fileoverview Example to compose response for rewrite of script-echo-response.
+ 
+  $request.url, $notify(title, subtitle, message), console.log(message), $done(response)
+ 
+  @supported Quantumult X (v1.0.3-build141)
+ */
 var url = $request.url;
 
 const myStatus = "HTTP/1.1 200 OK";
